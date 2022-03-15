821831d2ad910ac5f08ff74c38846405d53ca959
Christina Chan-Ying <christina.chanying@gmail.com>
2022-03-15 17:46:57 -0400

    Cleaned Data

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,40 +1,27 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
+# Purpose: Clean the 2020 Australian General Social Survey summary results data downloaded from the Australia Bureau of Statistics
 # Author: Christina Chan-Ying, Yandrickx Dumalog, Reese Halfyard
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Data: 15 March 2022
+# Contact: christina.chanying@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
 
 
 #### Workspace setup ####
-# Use R Projects, not setwd().
-# install.packages("xlsx")
-# install.packages("rio")
-# 
-# library(rio)
-# library(xlsx)
+# install.packages("tidyverse")
+# install.packages("readxl")
 
-install.packages("readxl")
 library(readxl)
-library(dplyr)
-library(haven)
 library(tidyverse)
 
 # Read in the raw data. 
+cleaned_data_males <- read_excel("inputs/data/GSS_Table3.xlsx", sheet = 1)
+cleaned_data_females <- read_excel("inputs/data/GSS_Table3.xlsx", sheet = 2)
+cleaned_data_persons <- read_excel("inputs/data/GSS_Table3.xlsx", sheet = 3)
 
-
-cleaned_data_males <- read_excel("inputs/data/GSS_Table3.xlsx", sheet = 2)
+# Save the cleaned data as csv files
 write_csv(cleaned_data_males, "inputs/data/cleaned_data_males.csv")
+write_csv(cleaned_data_females, "inputs/data/cleaned_data_females.csv")
+write_csv(cleaned_data_persons, "inputs/data/cleaned_data_persons.csv")
 
-
-install.packages("xlsx")
-library("xlsx")
-male_data <- read.xlsx("inputs/data/GSS_Table3.xlsx", 2, header=TRUE, skipEmptyRows = TRUE, colNames = TRUE, rowNames = TRUE)
-male_data
 
          
